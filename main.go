a543b3af94c8036fb3630362667082858b19b3a7
hanssu <rayhaniqbalbr@gmail.com>
2026-01-22 14:33:21 +0700

    fix: port

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -64,12 +65,13 @@ func main() {
 		})
 	})
 
-	fmt.Println("Server running di localhost:8080")
-
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("gagal running server")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // fallback lokal
 	}
+
+	log.Println("Server running on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func getProdukByID(w http.ResponseWriter, r *http.Request) {
